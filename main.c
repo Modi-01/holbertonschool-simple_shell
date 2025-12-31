1eafb2f66c256b08ad6c8435e384114b99fd32c2
adwaalmuraikhy <iadwaalmuraikhy@gmail.com>
2025-12-31 18:56:56 +0300

    Initialize owned environment copy at startup and free it on exit

--- main.c
+++ main.c
@@ -1,14 +1,30 @@
 #include "hsh.h"
 
 /**
- * main - Entry point
+ * main - entry point
  * @ac: argument count
  * @av: argument vector
  *
- * Return: shell status
+ * Return: 0 on success
  */
 int main(int ac, char **av)
 {
+	shell_ctx_t ctx;
+
 	(void)ac;
-	return (run_shell(av[0]));
+	ctx.av0 = av[0];
+	ctx.line_num = 0;
+	ctx.interactive = isatty(STDIN_FILENO);
+	ctx.should_exit = 0;
+	ctx.exit_status = 0;
+	ctx.env = NULL;
+
+	if (env_init(&ctx) == -1)
+		return (1);
+
+	shell_loop(&ctx);
+
+	env_free(&ctx);
+
+	return (0);
 }
